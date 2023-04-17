40af6a937dd036b706daa3897003f4fa9473445d
l.sarrazin <l.sarrazin@student.isartdigital.com>
2023-04-17 14:30:06 +0000

    L sarrazin

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,45 +1,64 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using Unity.VisualScripting;
+using UnityEditor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    CapsuleCollider m_Collider;
+
     [Header("Body")]
     [SerializeField]
     private Transform m_Feet;
 
-    [Header("Speed")]
+    [Header("Movement")]
     [SerializeField]
     private float m_Accelerate;
+
+    [SerializeField]
+    private float m_MaxSpeed;
 
     [SerializeField]
     private float m_DashPower;
 
     [SerializeField]
-    private float m_JumpHeight;
+    private float m_DashCooldownDuration;
+    private float m_DashCooldown;
 
     [SerializeField]
-    private float m_MaxSpeed;
+    private float m_MinJumpForce;
+
+    [SerializeField]
+    private float m_MaxJumpForce;
+
+    [SerializeField]
+    private float m_JumpTime;
+    private float m_JumpTimer;
+
+    private bool m_IsJumping = false;
+    private bool m_CanDoubleJump;
+
+    private bool m_IsDashing = false;
+    private float m_DashCd;
+
+    private Vector3 m_Move = new();
+    private Vector3 m_LastMove = new();
 
     [HideInInspector] 
     public byte PlayerLife = 10;
+
+    // To move into UI
+    [SerializeField]
+    TMP_Text m_PlayerLifeText;
 
     private Animator m_Animator;
 
     private Rigidbody m_Rigidbody;
 
-    private Vector3 m_Move = new();
-    private Vector3 m_LastMove = new();
-
-    private bool m_IsDahing = false;
-    private float m_DashCd;
-
-    private bool m_IsJumping = false;
-    public bool m_CanDoubleJump;
     private bool m_IsGrounded { get { return Physics.Raycast(m_Feet.transform.position, Vector3.down, 0.2f); } }
 
     private bool m_IsStuckLeft;
@@ -47,14 +66,67 @@ public class PlayerBehaviour : MonoBehaviour
     [HideInInspector]
     public bool m_HasTakenExplosion;
 
-    // To move into UI
+    [Header("Magnet")]
     [SerializeField]
-    TMP_Text m_PlayerLifeText;
+    private Transform m_Magnet;
+    private MagnetBehaviour m_MagnetBehaviour;
+
+    [SerializeField]
+    private float m_HoverTime;
+
+    [SerializeField]
+    private float m_ThrowForce;
+
+    [SerializeField]
+    private float m_ThrowTime;
+
+    [SerializeField]
+    private float m_PullTime;
+
+    private Vector2 m_Aim;
+
+    private bool HasMagnet { get { return m_Magnet.parent == transform; } }
+
+    private bool IsGrounded { get {
+            return Physics.Raycast(m_Feet.transform.position, Vector3.down, 0.05f)
+                || Physics.Raycast(m_Feet.transform.position + new Vector3(-m_Collider.radius, 0, 0), Vector3.down, 0.05f)
+                || Physics.Raycast(m_Feet.transform.position + new Vector3(m_Collider.radius, 0, 0), Vector3.down, 0.05f); } }
+
+    private bool m_IsStuckLeft;
+    private bool m_IsStuckRight;
+
+    private GameObject m_StandingOnObject { get {
+        bool center = Physics.Raycast(m_Feet.transform.position, Vector3.down, out RaycastHit CenterHit, 0.05f);
+        bool left = Physics.Raycast(m_Feet.transform.position + new Vector3(-m_Collider.radius, 0, 0), Vector3.down, out RaycastHit LeftHit, 0.05f);
+        bool right = Physics.Raycast(m_Feet.transform.position + new Vector3(m_Collider.radius, 0, 0), Vector3.down, out RaycastHit RightHit, 0.05f);
+        
+          /*
+        if (center)
+            return CenterHit.transform.gameObject;
+        if (left)
+            return LeftHit.transform.gameObject;
+        if (right)
+            return RightHit.transform.gameObject;
+        return null;
+        */
+
+        return center ? CenterHit.transform.gameObject : right ? RightHit.transform.gameObject : left ? LeftHit.transform.gameObject : null;
+    } }
+
+
+    private void OnValidate()
+    {
+        m_MagnetBehaviour = m_Magnet.GetComponent<MagnetBehaviour>();
+        m_MagnetBehaviour.HoverTime = m_HoverTime;
+        m_MagnetBehaviour.ThrowForce = m_ThrowForce;
+    }
 
     private void Awake()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<CapsuleCollider>();
+        m_MagnetBehaviour = m_Magnet.GetComponent<MagnetBehaviour>();
     }
 
     // Start is called before the first frame update
@@ -77,9 +149,27 @@ public class PlayerBehaviour : MonoBehaviour
             m_LastMove = m_Move;
         }
 
-        if (m_DashCd > 0)
+        if (m_DashCooldown > 0)
+            m_DashCooldown -= Time.deltaTime;
+
+        if (m_JumpTimer > 0)
+            m_JumpTimer -= Time.deltaTime;
+
+        if (!HasMagnet && m_MagnetBehaviour.HoverTimer <= 0)
         {
-            m_DashCd -= Time.deltaTime;
+            if (m_MagnetBehaviour.IsThrowing)
+            {
+                m_Magnet.GetComponent<BoxCollider>().enabled = false;
+                m_MagnetBehaviour.PullTime = m_PullTime;
+                m_MagnetBehaviour.TravelTimer = m_PullTime;
+                m_MagnetBehaviour.IsThrowing = false;
+            }
+            else if (m_MagnetBehaviour.TravelTimer <= 0)
+            {
+                m_Magnet.GetComponent<Rigidbody>().isKinematic = true;
+                m_Magnet.SetParent(transform, true);
+                m_Magnet.localPosition = new Vector3(0f, 0.2f, 1f);
+            }
         }
     }
     
@@ -107,14 +197,18 @@ public class PlayerBehaviour : MonoBehaviour
         m_IsStuckRight = false;
         m_IsStuckLeft = false;
 
-        if (m_IsDahing)
+        if (m_IsDashing)
             Dash();
 
         if (m_IsJumping)
             Jump();
 
-        if (m_IsGrounded)
+        if (IsGrounded)
         {
+            Rigidbody rigidbody = m_StandingOnObject.GetComponent<Rigidbody>();
+            if (rigidbody)
+                m_Rigidbody.velocity += rigidbody.velocity * Time.fixedDeltaTime;
+
             m_CanDoubleJump = true;
         }
     }
@@ -127,44 +221,95 @@ public class PlayerBehaviour : MonoBehaviour
 
     public void OnDash(InputAction.CallbackContext _context)
     {
-        if (_context.ReadValueAsButton() == true && m_DashCd <= 0)
+        if (_context.ReadValueAsButton() == true && m_DashCooldown <= 0)
         {
-            m_IsDahing = true;
-            m_DashCd = 2.0f;
+            m_IsDashing = true;
+            m_DashCooldown = m_DashCooldownDuration;
         }
     }
 
     public void OnJump(InputAction.CallbackContext _context)
     {
-        if (_context.ReadValueAsButton() == true && (m_IsGrounded == true || m_CanDoubleJump == true))
+        if (_context.ReadValueAsButton() == true && (IsGrounded == true || m_CanDoubleJump == true))
         {
             if (m_IsGrounded == false) 
-            { 
                 m_CanDoubleJump = false;
-            }
-            m_IsJumping = true;
+
+            m_Rigidbody.AddForce(Vector3.up * m_MinJumpForce, ForceMode.VelocityChange);
+            m_JumpTimer = m_JumpTime;
+            m_IsJumping = true; 
         }
+        else
+        {
+            m_IsJumping = false;
+        }
+    }
+
+    public void OnMagnetThrow(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            if (HasMagnet)
+            {
+                m_Magnet.transform.localPosition = new Vector3(0f, -0.2f, 0f);
+                m_Magnet.transform.rotation = new Quaternion(0, 0, 0, 0);
+                m_Magnet.SetParent(null, true);
+                m_Magnet.GetComponent<BoxCollider>().enabled = true;
+                m_Magnet.GetComponent<Rigidbody>().isKinematic = false;
+                m_Magnet.GetComponent<Rigidbody>().velocity = m_Rigidbody.velocity;
+
+                m_MagnetBehaviour.Aim = new Vector3(m_Aim.x, m_Aim.y, 0);
+                m_MagnetBehaviour.TravelTimer = m_ThrowTime;
+                m_MagnetBehaviour.HoverTimer = m_HoverTime;
+                m_MagnetBehaviour.IsThrowing = true;
+                m_MagnetBehaviour.ThrowForce = m_ThrowForce;
+            }
+            else if (m_MagnetBehaviour.TravelTimer <= 0)
+            {
+                m_Magnet.GetComponent<BoxCollider>().enabled = false; 
+                m_MagnetBehaviour.PullTime = m_PullTime;
+                m_MagnetBehaviour.TravelTimer = m_PullTime;
+                m_MagnetBehaviour.IsThrowing = false;
+                m_MagnetBehaviour.HoverTimer = 0;
+            }
+        }
+    }
+
+    public void OnAim(InputAction.CallbackContext _context)
+    {
+        if (GetComponent<PlayerInput>().defaultActionMap == "Keyboard")
+            m_Aim =  (_context.ReadValue<Vector2>() - new Vector2(Screen.width / 2f, Screen.height / 2f)).normalized;
+        else
+            m_Aim = _context.ReadValue<Vector2>().normalized;
     }
 
     public void Jump()
     {
-        Vector3 velocity = m_Rigidbody.velocity;
-        velocity.y = 0;
-        m_Rigidbody.velocity = velocity;
-        m_Rigidbody.AddForce(new Vector3(0, m_JumpHeight + Mathf.Abs(m_Rigidbody.velocity.y), 0), ForceMode.VelocityChange);
-        m_IsJumping = false;
+        if (!IsGrounded)
+        {
+            Vector3 velocity = m_Rigidbody.velocity;
+            velocity.y = 0;
+            m_Rigidbody.velocity = velocity;
+        }
+
+        m_Rigidbody.AddForce(Vector3.up * m_MaxJumpForce * 3f * (m_JumpTimer/(m_JumpTime*1.2f)) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+        if (m_JumpTimer < 0)
+            m_IsJumping = false;
     }
 
     public void Dash()
     {
+        m_Rigidbody.velocity = Vector3.zero;
         m_Rigidbody.AddForce(m_LastMove * m_DashPower, ForceMode.VelocityChange);
-        m_IsDahing = false;
+        m_IsDashing = false;
     }
 
     private void OnCollisionStay(Collision collision)
     {
         foreach (ContactPoint contact in collision.contacts) 
         {
+            // wall Collision
             if (Vector3.Angle(contact.normal, Vector3.up) == 90f)
             {
                 m_IsStuckLeft = Mathf.Sign(contact.normal.x) == 1;
